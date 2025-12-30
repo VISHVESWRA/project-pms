19675be6812b5eb2d4d38ead3d2093263b006010
VISHVESWRA <vishveswravichu@gmail.com>
2025-12-30 13:44:15 +0530

    fix: bug

--- src/app/axios.js
+++ src/app/axios.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  //   baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
   baseURL: "https://project-pms-server.onrender.com/api",
 });
 
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
+    // Make sure to set the correct header
+    req.headers = req.headers || {};
     req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
